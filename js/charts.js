025c559add88c4c85f24732baf2415f86066861c
LouLouBadillo <lulu.badillo.csm@gmail.com>
2020-07-18 15:12:13 -0500

    fix colors

--- js/charts.js
+++ js/charts.js
@@ -15,16 +15,16 @@ async function readData(){
                 label: 'Núcleo',
                 data: Object.values(poblacion[0]).filter(l=>l!="Nucleo"),
                 backgroundColor: [
-                    'rgba(255, 99, 132, 0.2)',
-                    'rgba(255, 99, 132, 0.2)',
-                    'rgba(255, 99, 132, 0.2)',
-                    'rgba(255, 99, 132, 0.2)',
+                    'rgba(1,110,196,0.8)',
+                    'rgba(1,110,196,0.8)',
+                    'rgba(1,110,196,0.8)',
+                    'rgba(1,110,196,0.8)',
                 ],
                 borderColor: [
-                    'rgba(255, 99, 132, 1)',
-                    'rgba(255, 99, 132, 1)',
-                    'rgba(255, 99, 132, 1)',
-                    'rgba(255, 99, 132, 1)',
+                    'rgba(1,110,196,1)',
+                    'rgba(1,110,196,1)',
+                    'rgba(1,110,196,1)',
+                    'rgba(1,110,196,1)',
 
                 ],
                 borderWidth: 1
@@ -33,17 +33,16 @@ async function readData(){
                 label: 'Complemento',
                 data: Object.values(poblacion[1]).filter(l=>l!="Complemento"),
                 backgroundColor: [
-                    'rgba(54, 162, 235, 0.2)',
-                    'rgba(54, 162, 235, 0.2)',
-                    'rgba(54, 162, 235, 0.2)',
-                    'rgba(54, 162, 235, 0.2)',
-                    'rgba(54, 162, 235, 0.2)',
+                    'rgba(49,192,219,0.8)',
+                    'rgba(49,192,219,0.8)',
+                    'rgba(49,192,219,0.8)',
+                    'rgba(49,192,219,0.8)',
                 ],
                 borderColor: [
-                    'rgba(54, 162, 235, 1)',
-                    'rgba(54, 162, 235, 1)',
-                    'rgba(54, 162, 235, 1)',
-                    'rgba(54, 162, 235, 1)'
+                    'rgba(49,192,219,1)',
+                    'rgba(49,192,219,1)',
+                    'rgba(49,192,219,1)',
+                    'rgba(49,192,219,1)'
                 ],
                 borderWidth: 1
             },
@@ -51,16 +50,16 @@ async function readData(){
                 label: 'Total',
                 data: Object.values(poblacion[2]).filter(l=>l!="Total"),
                 backgroundColor: [
-                    'rgba(255, 206, 86, 0.2)',
-                    'rgba(255, 206, 86, 0.2)',
-                    'rgba(255, 206, 86, 0.2)',
-                    'rgba(255, 206, 86, 0.2)'
+                    'rgba(61,180,179,0.8)',
+                    'rgba(61,180,179,0.8)',
+                    'rgba(61,180,179,0.8)',
+                    'rgba(61,180,179,0.8)'
                 ],
                 borderColor: [
-                    'rgba(255, 206, 86, 1)',
-                    'rgba(255, 206, 86, 1)',
-                    'rgba(255, 206, 86, 1)',
-                    'rgba(255, 206, 86, 1)'
+                    'rgba(61,180,179,1)',
+                    'rgba(61,180,179,1)',
+                    'rgba(61,180,179,1)',
+                    'rgba(61,180,179,1)'
                 ],
                 borderWidth: 1
             }]
@@ -85,20 +84,16 @@ async function readData(){
                 label: 'Núcleo',
                 data: Object.values(densidad[0]).filter(l=>l!="Nucleo"),
                 backgroundColor: [
-                    'rgba(255, 99, 132, 0.2)',
-                    'rgba(54, 162, 235, 0.2)',
-                    'rgba(255, 206, 86, 0.2)',
-                    'rgba(75, 192, 192, 0.2)',
-                    'rgba(153, 102, 255, 0.2)',
-                    'rgba(255, 159, 64, 0.2)'
+                    'rgba(1,110,196,0.8)',
+                    'rgba(1,110,196,0.8)',
+                    'rgba(1,110,196,0.8)',
+                    'rgba(1,110,196,0.8)',
                 ],
                 borderColor: [
-                    'rgba(255, 99, 132, 1)',
-                    'rgba(54, 162, 235, 1)',
-                    'rgba(255, 206, 86, 1)',
-                    'rgba(75, 192, 192, 1)',
-                    'rgba(153, 102, 255, 1)',
-                    'rgba(255, 159, 64, 1)'
+                    'rgba(1,110,196,1)',
+                    'rgba(1,110,196,1)',
+                    'rgba(1,110,196,1)',
+                    'rgba(1,110,196,1)',
                 ],
                 borderWidth: 1
             },
@@ -106,20 +101,16 @@ async function readData(){
                 label: 'Complemento',
                 data: Object.values(densidad[1]).filter(l=>l!="Complemento"),
                 backgroundColor: [
-                    'rgba(255, 99, 132, 0.2)',
-                    'rgba(54, 162, 235, 0.2)',
-                    'rgba(255, 206, 86, 0.2)',
-                    'rgba(75, 192, 192, 0.2)',
-                    'rgba(153, 102, 255, 0.2)',
-                    'rgba(255, 159, 64, 0.2)'
+                    'rgba(49,192,219,0.8)',
+                    'rgba(49,192,219,0.8)',
+                    'rgba(49,192,219,0.8)',
+                    'rgba(49,192,219,0.8)',
                 ],
                 borderColor: [
-                    'rgba(255, 99, 132, 1)',
-                    'rgba(54, 162, 235, 1)',
-                    'rgba(255, 206, 86, 1)',
-                    'rgba(75, 192, 192, 1)',
-                    'rgba(153, 102, 255, 1)',
-                    'rgba(255, 159, 64, 1)'
+                    'rgba(49,192,219,1)',
+                    'rgba(49,192,219,1)',
+                    'rgba(49,192,219,1)',
+                    'rgba(49,192,219,1)'
                 ],
                 borderWidth: 1
             },
@@ -127,20 +118,16 @@ async function readData(){
                 label: 'Total',
                 data: Object.values(densidad[2]).filter(l=>l!="Total"),
                 backgroundColor: [
-                    'rgba(255, 99, 132, 0.2)',
-                    'rgba(54, 162, 235, 0.2)',
-                    'rgba(255, 206, 86, 0.2)',
-                    'rgba(75, 192, 192, 0.2)',
-                    'rgba(153, 102, 255, 0.2)',
-                    'rgba(255, 159, 64, 0.2)'
+                    'rgba(61,180,179,0.8)',
+                    'rgba(61,180,179,0.8)',
+                    'rgba(61,180,179,0.8)',
+                    'rgba(61,180,179,0.8)'
                 ],
                 borderColor: [
-                    'rgba(255, 99, 132, 1)',
-                    'rgba(54, 162, 235, 1)',
-                    'rgba(255, 206, 86, 1)',
-                    'rgba(75, 192, 192, 1)',
-                    'rgba(153, 102, 255, 1)',
-                    'rgba(255, 159, 64, 1)'
+                    'rgba(61,180,179,1)',
+                    'rgba(61,180,179,1)',
+                    'rgba(61,180,179,1)',
+                    'rgba(61,180,179,1)'
                 ],
                 borderWidth: 1
             }]
@@ -165,20 +152,22 @@ async function readData(){
                 label: 'Núcleo',
                 data: Object.values(extension[0]).filter(l=>l!="Nucleo"),
                 backgroundColor: [
-                    'rgba(255, 99, 132, 0.2)',
-                    'rgba(54, 162, 235, 0.2)',
-                    'rgba(255, 206, 86, 0.2)',
-                    'rgba(75, 192, 192, 0.2)',
-                    'rgba(153, 102, 255, 0.2)',
-                    'rgba(255, 159, 64, 0.2)'
+                    'rgba(1,110,196,0.8)',
+                    'rgba(1,110,196,0.8)',
+                    'rgba(1,110,196,0.8)',
+                    'rgba(1,110,196,0.8)',
+                    'rgba(1,110,196,0.8)',
+                    'rgba(1,110,196,0.8)',
+                    'rgba(1,110,196,0.8)'
                 ],
                 borderColor: [
-                    'rgba(255, 99, 132, 1)',
-                    'rgba(54, 162, 235, 1)',
-                    'rgba(255, 206, 86, 1)',
-                    'rgba(75, 192, 192, 1)',
-                    'rgba(153, 102, 255, 1)',
-                    'rgba(255, 159, 64, 1)'
+                    'rgba(1,110,196,1)',
+                    'rgba(1,110,196,1)',
+                    'rgba(1,110,196,1)',
+                    'rgba(1,110,196,1)',
+                    'rgba(1,110,196,1)',
+                    'rgba(1,110,196,1)',
+                    'rgba(1,110,196,1)'
                 ],
                 borderWidth: 1
             },
@@ -186,20 +175,22 @@ async function readData(){
                 label: 'Complemento',
                 data: Object.values(extension[1]).filter(l=>l!="Complemento"),
                 backgroundColor: [
-                    'rgba(255, 99, 132, 0.2)',
-                    'rgba(54, 162, 235, 0.2)',
-                    'rgba(255, 206, 86, 0.2)',
-                    'rgba(75, 192, 192, 0.2)',
-                    'rgba(153, 102, 255, 0.2)',
-                    'rgba(255, 159, 64, 0.2)'
+                    'rgba(49,192,219,0.8)',
+                    'rgba(49,192,219,0.8)',
+                    'rgba(49,192,219,0.8)',
+                    'rgba(49,192,219,0.8)',
+                    'rgba(49,192,219,0.8)',
+                    'rgba(49,192,219,0.8)',
+                    'rgba(49,192,219,0.8)'
                 ],
                 borderColor: [
-                    'rgba(255, 99, 132, 1)',
-                    'rgba(54, 162, 235, 1)',
-                    'rgba(255, 206, 86, 1)',
-                    'rgba(75, 192, 192, 1)',
-                    'rgba(153, 102, 255, 1)',
-                    'rgba(255, 159, 64, 1)'
+                    'rgba(49,192,219,1)',
+                    'rgba(49,192,219,1)',
+                    'rgba(49,192,219,1)',
+                    'rgba(49,192,219,1)',
+                    'rgba(49,192,219,1)',
+                    'rgba(49,192,219,1)',
+                    'rgba(49,192,219,1)'
                 ],
                 borderWidth: 1
             },
@@ -207,20 +198,22 @@ async function readData(){
                 label: 'Total',
                 data: Object.values(extension[2]).filter(l=>l!="Total"),
                 backgroundColor: [
-                    'rgba(255, 99, 132, 0.2)',
-                    'rgba(54, 162, 235, 0.2)',
-                    'rgba(255, 206, 86, 0.2)',
-                    'rgba(75, 192, 192, 0.2)',
-                    'rgba(153, 102, 255, 0.2)',
-                    'rgba(255, 159, 64, 0.2)'
+                    'rgba(61,180,179,0.8)',
+                    'rgba(61,180,179,0.8)',
+                    'rgba(61,180,179,0.8)',
+                    'rgba(61,180,179,0.8)',
+                    'rgba(61,180,179,0.8)',
+                    'rgba(61,180,179,0.8)',
+                    'rgba(61,180,179,0.8)'
                 ],
                 borderColor: [
-                    'rgba(255, 99, 132, 1)',
-                    'rgba(54, 162, 235, 1)',
-                    'rgba(255, 206, 86, 1)',
-                    'rgba(75, 192, 192, 1)',
-                    'rgba(153, 102, 255, 1)',
-                    'rgba(255, 159, 64, 1)'
+                    'rgba(61,180,179,1)',
+                    'rgba(61,180,179,1)',
+                    'rgba(61,180,179,1)',
+                    'rgba(61,180,179,1)',
+                    'rgba(61,180,179,1)',
+                    'rgba(61,180,179,1)',
+                    'rgba(61,180,179,1)'
                 ],
                 borderWidth: 1
             }]
