0944f75c4ec6ac05e8454d592fc302a03921c32b
Eduardo Villalpando Mello <eduardo.villalpando.mello@gmail.com>
2020-08-19 19:58:25 -0500

    Revert "Altas2 (#32)"
    This reverts commit cb68bfd5558e177139856e47d1ffbd9e80805729.

--- js/charts.js
+++ js/charts.js
@@ -263,15 +263,77 @@ async function readData(){
     //Costos
     var costContainer = document.getElementById('costChart');
     var costChart = new Chart(costContainer, {
-        type: 'line',
+        type: 'bar',
         data: {
             labels: Object.keys(extension[2]).filter(l=>l!=""),
-            datasets: [
+            datasets: [{
+                label: 'Costo de pavimentación',
+                data: Object.values(extension[0]).filter(l=>l!="Nucleo"),
+                backgroundColor: [
+                    'rgba(252, 186, 128)',
+                    'rgba(252, 186, 128)',
+                    'rgba(252, 186, 128)',
+                    'rgba(252, 186, 128)',
+                    'rgba(252, 186, 128)',
+                    'rgba(252, 186, 128)',
+                    'rgba(252, 186, 128)'
+                ],
+                borderColor: [
+                    'rgba(252, 186, 128)',
+                    'rgba(252, 186, 128)',
+                    'rgba(252, 186, 128)',
+                    'rgba(252, 186, 128)',
+                    'rgba(252, 186, 128)',
+                    'rgba(252, 186, 128)',
+                    'rgba(252, 186, 128)'
+                ],
+                borderWidth: 1
+            },
             {
-                fill: 'origin',
+                label: 'Tomas de agua',
+                data: Object.values(extension[1]).filter(l=>l!="Complemento"),
+                backgroundColor: [
+                    'rgba(243, 119, 94)',
+                    'rgba(243, 119, 94)',
+                    'rgba(243, 119, 94)',
+                    'rgba(243, 119, 94)',
+                    'rgba(243, 119, 94)',
+                    'rgba(243, 119, 94)',
+                    'rgba(243, 119, 94)'
+                ],
+                borderColor: [
+                    'rgba(243, 119, 94)',
+                    'rgba(243, 119, 94)',
+                    'rgba(243, 119, 94)',
+                    'rgba(243, 119, 94)',
+                    'rgba(243, 119, 94)',
+                    'rgba(243, 119, 94)',
+                    'rgba(243, 119, 94)'
+                ],
+                borderWidth: 1
+            },
+            {
                 label: 'Total',
                 data: Object.values(extension[2]).filter(l=>l!="Total"),
-                backgroundColor: '#f3775e',
+                backgroundColor: [
+                    'rgba(210, 65, 111)',
+                    'rgba(210, 65, 111)',
+                    'rgba(210, 65, 111)',
+                    'rgba(210, 65, 111)',
+                    'rgba(210, 65, 111)',
+                    'rgba(210, 65, 111)',
+                    'rgba(210, 65, 111)'
+                ],
+                borderColor: [
+                    'rgba(210, 65, 111)',
+                    'rgba(210, 65, 111)',
+                    'rgba(210, 65, 111)',
+                    'rgba(210, 65, 111)',
+                    'rgba(210, 65, 111)',
+                    'rgba(210, 65, 111)',
+                    'rgba(210, 65, 111)'
+                ],
+                borderWidth: 1
             }]
         },
         options: {
